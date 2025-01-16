72ac6704907af454875faf75625a4d4fc79fd4d2
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-01-16 11:32:50 -0600

    :sparkles: Add challenge 04 in js and ts

---
+++ src/challenge-04/index.js
@@ -0,0 +1,16 @@
+function createXmasTree(height, ornament) {
+  let res = "";
+
+  for (let index = height; index > 0; index--) {
+    res += "_".repeat(index - 1);
+    res += ornament.repeat((height - index) * 2 + 1);
+    res += "_".repeat(index - 1) + "\n";
+  }
+
+  res += `${"_".repeat(height - 1)}#${"_".repeat(height - 1)}\n`;
+  res += `${"_".repeat(height - 1)}#${"_".repeat(height - 1)}`;
+  
+  return res;
+}
+
+module.exports = createXmasTree;
