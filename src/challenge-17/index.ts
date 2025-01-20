3cee7991885d267dc02f4dded6daf8ba2f211836
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-01-19 21:16:02 -0600

    :sparkles: Add challenge 17 in js and ts

---
+++ src/challenge-17/index.ts
@@ -0,0 +1,21 @@
+export function detectBombs(grid: boolean[][]): number[][] {
+  let res: number[][] = [];
+
+  function addBomb(i: number, j: number) {
+    return (grid[i] ?? [false])[j] ? 1 : 0;
+  }
+
+  for (let i = 0; i < grid.length; i++) {
+    res.push([]);
+
+    for (let j = 0; j < grid[i].length; j++) {
+      res[i][j] = 0;
+      
+      res[i][j] += addBomb(i - 1, j - 1) + addBomb(i - 1, j) + addBomb(i - 1, j + 1);
+      res[i][j] += addBomb(i, j - 1) + addBomb(i, j + 1);
+      res[i][j] += addBomb(i + 1, j - 1) + addBomb(i + 1, j) + addBomb(i + 1, j + 1);
+    }
+  }
+
+  return res;
+}
