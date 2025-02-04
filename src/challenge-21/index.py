9e19eb1c467b5d5b422ecb96f6b4c8e74d1accd3
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-02-03 22:57:46 -0600

    :sparkles: Add challenge 21 in python

---
+++ src/challenge-21/index.py
@@ -0,0 +1,19 @@
+from typing import TypedDict, NotRequired, Optional
+
+BinaryTree = TypedDict(
+  "BinaryTree",
+  {
+    "value": str,
+    "left": NotRequired["BinaryTree"],
+    "right": NotRequired["BinaryTree"],
+  },
+)
+
+def tree_height(tree: Optional[BinaryTree]) -> int:
+  if tree is None:
+    return 0
+
+  left_height: int = tree_height(tree=tree["left"]) if "left" in tree else 0
+  right_height: int = tree_height(tree=tree["right"]) if "right" in tree else 0
+
+  return 1 + max(left_height, right_height)
