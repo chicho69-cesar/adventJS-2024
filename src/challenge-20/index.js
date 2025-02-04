cf551e5df8ad12c03b2e099551492fd077fbfd49
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-02-03 22:55:23 -0600

    :sparkles: Add challenge 20 in js and ts

---
+++ src/challenge-20/index.js
@@ -0,0 +1,33 @@
+function fixGiftList(received, expected) {
+  const receivedDict = {};
+  const expectedDict = {};
+
+  received.forEach((val) => {
+    receivedDict[val] ||= 0;
+    receivedDict[val] += 1;
+  });
+
+  expected.forEach((val) => {
+    expectedDict[val] ||= 0;
+    expectedDict[val] += 1;
+  });
+
+  const missingDict = { ...expectedDict };
+  const extraDict = {};
+
+  Object.keys(receivedDict).forEach((val) => {
+    const fits = (expectedDict[val] ?? 0) - receivedDict[val];
+    delete missingDict[val];
+    
+    if (fits < 0) {
+      extraDict[val] = Math.abs(fits);
+    } else if (fits > 0) {
+      missingDict[val] = fits;
+    }
+  });
+
+  return {
+    missing: missingDict,
+    extra: extraDict,
+  };
+}
